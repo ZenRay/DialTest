11b4976481514389691bd6257bddacba31dc639d
ZenRay <jake2011ren@gmail.com>
2021-02-04 19:02:14 +0800

    feat($service $request_dial): Request Dial Task
    异步请求播测任务

--- source/app/utils/service/__init__.py
+++ source/app/utils/service/__init__.py
@@ -3,4 +3,4 @@ __all__ = ["Heart", "DialTask"]
 
 
 from .heatbeat import Heart, NORM_RATE, ABNORM_RATE
-from ._dial_task import DialTask
+from ._request_task import DialTask
---
+++ source/app/utils/service/_request_task.py
@@ -0,0 +1,29 @@
+#coding:utf8
+"""
+播测任务请求
+"""
+import asyncio
+import logging
+import urllib3
+import aiohttp
+import time
+
+from urllib3 import exceptions
+from aiohttp import web
+
+from ..check import _url_valid, _args_valid
+from ._base import Request
+
+logger = logging.getLogger("app.utils.service.dialtask")
+
+
+
+class DialTask(Request):
+    """播测任务类
+
+    继承异步 Request 类
+    """
+    async def request(self, *, url=None, method="POST", data=None):
+        """发出播测任务请求"""
+        result = await super().request(url=url, method=method, data=data)
+        return result
